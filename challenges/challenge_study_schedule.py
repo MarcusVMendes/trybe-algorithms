6b9862f4e36267d002421c5d0cb080c7ad1308e9
MarcusVMendes <marcus.mendes89@gmail.com>
2022-04-22 22:02:22 -0300

    Requisito 1 concluido

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        count = 0
+        for item in permanence_period:
+            if item[0] <= target_time <= item[1]:
+                count += 1
+        # print(count)
+        return count
+    except Exception:
+        return None
