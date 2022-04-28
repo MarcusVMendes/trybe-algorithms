c02f149540739b467e1d86ce70ed7c0a6e7bd3a0
MarcusVMendes <marcus.mendes89@gmail.com>
2022-04-28 20:14:20 -0300

    Requisito 3 concluido

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    l1 = list(first_string.lower())
+    l2 = list(second_string.lower())
+
+    if len(l1) != len(l2):
+        return False
+
+    for letter in l1:
+        if letter in l2:
+            l2.remove(letter)
+        else:
+            return False
+    return True
